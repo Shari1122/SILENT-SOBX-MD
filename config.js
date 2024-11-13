f105bedba03f4fa3c2619fafa150d9e4168a2060
Shari1122 <144144482+Shari1122@users.noreply.github.com>
2024-11-13 08:50:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "fqRUTYjK#OvvArR9NN-N0Up0ClxYIt49pXnycLdHkq-kKYVO_Gds",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
@@ -16,9 +16,9 @@ ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATS
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-OWNER_REACT: process.env.OWNER_REACT || "true",
-ALWAYS_OFFLINE: process.env.ALWAYS_OFFLINE || "true",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
+ALWAYS_OFFLINE: process.env.ALWAYS_OFFLINE || "false",
 BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
 };
